53d4bf1e9319d444640858bcb29028802bd702d8
Replit user <>
2023-03-04 21:55:41 -0700

    Form styling

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -7,6 +7,13 @@ function MessageForm() {
 
     return (
         <form id="message-form" onSubmit={handleFormSubmit}>
+            <label className='formLabel'>To (ETH address)</label>
+            <input type='text' placeholder="0xB01A..." className='formField'></input>
+            <label className='formLabel'>Subject</label>
+            <input type='text' placeholder="Subject" className='formField'></input>
+            <label className='formLabel'>Message</label>
+            <textarea placeholder="Message" className='formField' rows='20' ></textarea>
+            <button type="submit">Send <i>[]</i></button>
         </form>
     );
 }
