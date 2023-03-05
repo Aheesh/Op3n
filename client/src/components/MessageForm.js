394f7535eec7d39c465cf4fff3dc85feb7852e85
Replit user <>
2023-03-04 22:02:13 -0700

    Form functionality

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 function MessageForm() {
+    const [formData, setFormData] = useState({
+        to: '',
+        subject: '',
+        content: ''
+    })
+
+    const handleInputChange = (event) => {
+        const { id, value } = event.target;
+        setFormData({ ...formData, [id]: value });
+      };
+    
+
     const handleFormSubmit = (event) => {
         event.preventDefault();
+        //SEND MESSAGE USING STATE DATA
       };
 
     return (
         <form id="message-form" onSubmit={handleFormSubmit}>
-            <label className='formLabel'>To (ETH address)</label>
-            <input type='text' placeholder="0xB01A..." className='formField'></input>
-            <label className='formLabel'>Subject</label>
-            <input type='text' placeholder="Subject" className='formField'></input>
-            <label className='formLabel'>Message</label>
-            <textarea placeholder="Message" className='formField' rows='20' ></textarea>
+            <label htmlFor='to' className='formLabel'>To (ETH address)</label>
+            <input id='to' type='text' placeholder="0xB01A..." className='formField' onChange={handleInputChange}></input>
+            <label htmlFor='subject' className='formLabel'>Subject</label>
+            <input id='subject' type='text' placeholder="Subject" className='formField' onChange={handleInputChange}></input>
+            <label htmlFor='content' className='formLabel'>Message</label>
+            <textarea id='content' placeholder="Message" className='formField' rows='20' onChange={handleInputChange}></textarea>
             <button type="submit">Send <i>[]</i></button>
         </form>
     );
